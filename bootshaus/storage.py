6d4cbaf605664833be13c5b3b614e1a25b633bc2
Fabian Thylmann <fthylmann@me.com>
2023-03-13 12:07:04 +0100

    Updated config and added boto3

--- bootshaus/storage.py
+++ bootshaus/storage.py
@@ -2,11 +2,11 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticStorage(S3Boto3Storage):
     bucket_name = 'bootshaus-id-storage'
-    custom_domain = '{}.nyc3.digitaloceanspaces.com'.format(bucket_name)
+    custom_domain = '{}.ams3.digitaloceanspaces.com'.format(bucket_name)
     location = 'static'
 
 class MediaStorage(S3Boto3Storage):
     bucket_name = 'bootshaus-id-storage'
-    custom_domain = '{}.nyc3.digitaloceanspaces.com'.format(bucket_name)
+    custom_domain = '{}.ams3.digitaloceanspaces.com'.format(bucket_name)
     location = 'media'
 
